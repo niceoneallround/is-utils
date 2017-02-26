987a584c9b76d1e51ef223c51962297ecbd13a92
niceoneallround <niceoneallround@gmail.com>
2017-02-25 16:13:27 -0800

    Add the code to validate query and create an internal query

--- messages/lib/Query.js
+++ messages/lib/Query.js
@@ -8,27 +8,31 @@ IS queries are a JSON representation of the public input query
 
 
 {
-   @graph:[ {
-     @id: http://pn.id.webshield.io/query/com/acme#73733737
-     @type: https://pn.schema.webshield.io/type#query,
-     pn_p.params: {
-       @id: ...
-       @type: http://pn.schema.webshield.io/type#SubjectQueryRestriction,
-       http://pn.schema.webshield.io/prop#subjectID: value
-       http://pn.schema.webshield.io/prop#subject_type: value
-       ... other query params
-     },
-     pn_p.properties: {
-       familyName:
-       givenName:
-       taxID:
-     },
-     result_graph_property: 'bob',
+   @id: http://pn.id.webshield.io/query/com/acme#73733737
+   @type: https://pn.schema.webshield.io/type#subject_query,
+   query_nodes: [
+     { id: blank node id
+       type: queryNode,
+       result_graph_property: 'bob',
+       pn_p.params:{
+         @id: ...
+         @type: http://pn.schema.webshield.io/type#SubjectQueryRestriction, // note need this type as the privacy algorithm applies to this
+         http://pn.schema.webshield.io/prop#subjectID: value
+         http://pn.schema.webshield.io/prop#subject_type: value
+         ... other query params
+       },
+       pn_p.properties: {
+        familyName:
+        givenName:
+        taxID:
+     }
+  ]
   }
  }
 */
 
 const assert = require('assert');
+const JSONLDUtils = require('jsonld-utils/lib/jldUtils');
 const moment = require('moment');
 const PNDataModel = require('data-models/lib/PNDataModel');
 const PN_P = PNDataModel.PROPERTY;
@@ -44,13 +48,61 @@ function nextIdCounter() {
 
 class Query {
 
-  static createJSONFromPublicJSON(hostname) {
-    assert(hostname, 'createJSON hostname param is missing');
+  static createJSONFromPublicJSON(publicQry, hostname) {
+    assert(hostname, 'createJSONFromPublicJSON hostname param is missing');
+    assert(publicQry, 'createJSONFromPublicJSON hostname param is missing');
+
+    let err = Query.validatePublicJSON(publicQry, hostname);
+    if (err) {
+      throw err;
+    }
 
     let qry = {
-      '@id': PNDataModel.ids.createQueryId(hostname, nextIdCounter()),
-      '@type': PN_T.Query,
-      [PN_P.description]: 'Hard coded to get flow workingf does not reflect results',
+      '@id': PNDataModel.ids.createQueryId(hostname, publicQry.id),
+      '@type': PN_T.SubjectQuery,
+      [PN_P.queryNodes]: [],
+    };
+
+    if (publicQry.description) {
+      qry[PN_P.description] = publicQry.description;
+    }
+
+    // find all the top level properties of graph and for each one create a query node as described above.
+    let keys = Object.keys(publicQry.graph);
+
+    for (let i = 0; i < keys.length; i++) {
+      let keyData = publicQry.graph[keys[i]];
+
+      let node = JSONLDUtils.createBlankNode({ '@type': PN_T.QueryNode, });
+      node[PN_P.queryResultGraphNode] = keys[i];
+
+      // create the query restriction based on the params for now hard coded to
+      // just use the id
+      // let paramKeys = Object.keys(keyData.__params);
+      node[PN_P.params] = {
+        '@id': PNDataModel.ids.createQueryRestrictionId(hostname, nextIdCounter()),
+        '@type': PN_T.SubjectQueryRestriction,
+        [PN_P.subjectID]: keyData.__params.id,
+      };
+
+      // add the properties that are required -  note not an array as want to keep the shape and use compact/expand
+      // FIXME add code to handle embedded types
+      //
+      node[PN_P.properties] = {};
+      let properties =  Object.keys(keyData); // the keys within the object are the properties that are needed. Skip __params
+      for (let j = 0; j < properties.length; j++) {
+        if ((properties[j] !== '__params') && (properties[j] !== '__quality')) {
+
+          // FIXME add code to handle embedded objects if the property is an object
+          node[PN_P.properties][properties[j]] = '';
+        }
+      }
+
+      qry[PN_P.queryNodes].push(node);
+    }
+
+    /*
+
       [PN_P.params]: {
         '@id': PNDataModel.ids.createQueryRestrictionId(hostname, nextIdCounter()),
         '@type': PN_T.SubjectQueryRestriction,
@@ -66,7 +118,7 @@ class Query {
           },
       },
       [PN_P.queryResultGraphProp]: 'bob',
-    };
+    };*/
 
     return qry;
   }
@@ -94,11 +146,37 @@ class Query {
       });
     }
 
-    if (!query.subjects) {
+    if (!query.graph) {
       return PNDataModel.errors.createTypeError({
         id: PNDataModel.ids.createErrorId(hostname, nextIdCounter()),
-        errMsg: util.format('ERROR no subjects in query object:%j', query),
+        errMsg: util.format('ERROR no graph property in query object:%j', query),
       });
+    }
+
+    let keys = Object.keys(query.graph);
+    if (keys.length === 0) {
+      return PNDataModel.errors.createTypeError({
+        id: PNDataModel.ids.createErrorId(hostname, nextIdCounter()),
+        errMsg: util.format('ERROR graph does not have any properties:%j', query),
+      });
+    }
+
+    if (keys.length !== 1) {
+      return PNDataModel.errors.createTypeError({
+        id: PNDataModel.ids.createErrorId(hostname, nextIdCounter()),
+        errMsg: util.format('ERROR graph has more than one property can only handle one property for now:%j', query),
+      });
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+      let keyData = query.graph[keys[i]];
+
+      if (!keyData.__params) {
+        return PNDataModel.errors.createTypeError({
+          id: PNDataModel.ids.createErrorId(hostname, nextIdCounter()),
+          errMsg: util.format('ERROR graph has no params data for key:%s :%j', keys[i], query),
+        });
+      }
     }
 
     return null;
@@ -114,15 +192,15 @@ class Query {
 
     // this is query by an id
     return {
-      id: 'abc',
-      type: 'Query',
-      subjects: {
+      id: 'test-query-by-id',
+      type: 'SubjectQuery',
+      graph: {
         bob: {
           __params: { id: queryId, }, // query params
           __quality: {}, // future quality attributes needed by provider
           familyName: '',
           givenName: '',
-          taxId: '',
+          taxID: '',
         },
       },
     };
--- messages/test/testQuery.js
+++ messages/test/testQuery.js
@@ -1,66 +1,39 @@
 /*jslint node: true, vars: true */
-
-const assert = require('assert');
 const PNDataModel = require('data-models/lib/PNDataModel');
 const PN_P = PNDataModel.PROPERTY;
 const PN_T = PNDataModel.TYPE;
 const Query = require('../lib/Query');
-const util = require('util');
 
 describe('1 create JSON messages tests', function () {
   'use strict';
 
-  it('1.1 should create a valid json message', function () {
+  it('1.1 should create an internal subject query with all the public query information', function () {
 
-    let mess = Query.createJSONFromPublicJSON('fakehostname.com');
+    let publicJSON = Query.createPublicJSONCanonById(); // use default id
+
+    let mess = Query.createJSONFromPublicJSON(publicJSON, 'fakehostname.com');
     console.log(mess);
     mess.should.have.property('@id');
-    mess.should.have.property('@type');
+    mess.should.have.property('@type', PN_T.SubjectQuery);
+    mess.should.have.property(PN_P.queryNodes);
+    mess[PN_P.queryNodes].length.should.be.equal(1);
 
-    mess.should.have.property(PN_P.params);
-    mess[PN_P.params].should.have.property('@id');
-    mess[PN_P.params].should.have.property('@type', PN_T.SubjectQueryRestriction);
-    mess[PN_P.params].should.have.property(PN_P.subjectID);
+    let node = mess[PN_P.queryNodes][0];
+    node.should.have.property('@id');
+    node.should.have.property('@type', [PN_T.QueryNode]);
+    node.should.have.property(PN_P.queryResultGraphNode, 'bob');
 
-    mess.should.have.property(PN_P.properties);
-    let keys = Object.keys(mess[PN_P.properties]);
+    node.should.have.property(PN_P.params);
+    node[PN_P.params].should.have.property('@id');
+    node[PN_P.params].should.have.property('@type', PN_T.SubjectQueryRestriction);
+    node[PN_P.params].should.have.property(PN_P.subjectID);
+
+    node.should.have.property(PN_P.properties);
+    let keys = Object.keys(node[PN_P.properties]);
     keys.should.be.eql([
-      'https://schema.org/familyName',
-      'https://schema.org/givenName',
-      'https://schema.org/taxID',
-      'https://schema.org/address',
+      'familyName',
+      'givenName',
+      'taxID',
     ]);
-
-    for (let i = 0; i < keys.length; i++) {
-
-      switch (typeof mess[PN_P.properties][keys[i]]) {
-
-        case 'string': {
-          if ((keys[i] !== 'https://schema.org/familyName') &&
-             (keys[i] !== 'https://schema.org/givenName') &&
-             (keys[i] !== 'https://schema.org/taxID')) {
-            assert(false, util.format('string key is unexpected:%j', keys[i]));
-          }
-
-          break;
-        }
-
-        case 'object': {
-          keys[i].should.be.equal('https://schema.org/address');
-          break;
-        }
-
-        default: {
-          console.log(typeof mess[PN_P.properties][keys[i]]);
-          assert(false, util.format('key is not a string or object:%s - typeof:%s',
-                  keys[i],
-                  (typeof mess[PN_P.properties][keys[i]])
-                )
-              );
-        }
-      }
-    }
-
-    mess.should.have.property(PN_P.queryResultGraphProp, 'bob');
   }); // 1.1
 }); // describe 1
